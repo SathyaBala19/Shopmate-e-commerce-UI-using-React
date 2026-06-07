29335d227d1885e0ae7d29dde337b9639b263c1a
SathyaBala19 <sathyabala1934@gmail.com>
2026-06-07 16:05:56 +0530

    commit

---
+++ shopmate-store/src/features/products/productsData.js
@@ -0,0 +1,32 @@
+const products = [
+    {
+        id: 1,
+        name: "Men's Knitwear",
+        price: 14.99,
+        image: "https://picsum.photos/200?1",
+        category: "Men"
+    },
+    {
+        id: 2,
+        name: "Gradient Tee",
+        price: 14.99,
+        image: "https://picsum.photos/200?2",
+        category: "Men"
+    },
+    {
+        id: 3,
+        name: "Novelty T-Shirt",
+        price: 14.99,
+        image: "https://picsum.photos/200?3",
+        category: "Men"
+    },
+    {
+        id: 4,
+        name: "Harajuku Shirt",
+        price: 14.99,
+        iamge: "https://picsum.photos/200?4",
+        category: "Men"
+    }
+];
+
+export default products;
